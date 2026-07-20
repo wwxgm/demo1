d9788721399406f65af5485232b0bafec39f2021
wwx <547065931@qq.com>
2026-07-20 18:56:08 +0800

    test: add app widget smoke test

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+﻿import 'package:flutter_test/flutter_test.dart';
+import 'package:autovi/main.dart';
+
+void main() {
+  testWidgets('shows single video flip action', (tester) async {
+    await tester.pumpWidget(const AutoVideoFlipApp());
+
+    expect(find.text('自动翻转视频'), findsOneWidget);
+    expect(find.text('选择视频并自动翻转'), findsOneWidget);
+  });
+}
